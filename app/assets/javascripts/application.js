8d2fc7d6cfd3d12b67e7ab6ee1776bab01158768
Ben Fischer <ben.fischer.810@gmail.com>
2013-12-24 12:34:59 -0600

    scaffold out directory structure for scheduler angualr app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require angular
+//= require scheduler
 //= require_tree .
 
 $(function(){
---
+++ app/assets/javascripts/scheduler/app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var schedulerApp = angular.module('schedulerApp', []);
