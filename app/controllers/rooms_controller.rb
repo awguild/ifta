f6fc0c5db5c561c19a8f32b438931c95f6c32bd1
Ben Fischer <ben.fischer.810@gmail.com>
2013-12-26 07:44:26 -0600

    make rooms controller respond to json

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,5 +4,9 @@ class RoomsController < ApplicationController
 	    @schedule = @conference.schedule
 	    @rooms = @schedule.rooms
 	    authorize! :edit, @schedule
+        respond_to do |format|
+            format.html #index.html.erb
+            format.json { render json: @rooms}
+        end
 	end
 end
