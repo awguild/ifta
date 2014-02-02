e4715d80010219735fb1db591aa35da727699fd7
Ben Fischer <ben.fischer.810@gmail.com>
2014-02-02 10:37:37 -0600

    set up routing for app

--- app/assets/javascripts/scheduler/app.js
+++ app/assets/javascripts/scheduler/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var schedulerApp = angular.module('schedulerApp', ['ngResource', 'ngRoute']);
+schedulerApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+   $routeProvider.when('/rooms', {
+        templateUrl: '/assets/scheduler/views/rooms/edit.html', 
+        controller: 'RoomsCtrl' 
+    })
+   .otherwise({
+        redirectTo: '/presentations'
+    });
+}]);
