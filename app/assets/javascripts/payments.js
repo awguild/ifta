949300405df8167eee971ab7d20e4625c8d807eb
mattl202 <matthewleja11@augustana.edu>
2014-09-15 16:47:28 -0500

    Finish implementing ajax remove transactions

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -1,12 +1,19 @@
 $(function() {
     var forms = $('.deleteTransaction').closest("form")
       , hasDeleteTransactionForms = forms.length;
-      console.log(forms);
-
     if (hasDeleteTransactionForms) {
       forms.submit(function(e) {
+        var section = $(e.target).closest(".unpaid");
         e.preventDefault();
-        console.log("Event happened ", e);
+        $.ajax(e.target.attributes.action.value,{
+            type: "DELETE"
+        })
+        .done(function(data) {
+            section.hide(1000);
+        })
+        .fail(function(err) {
+            section.append("Opps, we couldn't remove this transaction.");
+        });
       });
     }
 });
