79d3e7d9223a703fd4e1a69a4a20995b552bb9fd
Ben Fischer <ben.fischer.810@gmail.com>
2015-05-27 13:03:43 -0500

    shorter syntax for module setup

--- app/assets/javascripts/scheduler/app.js
+++ app/assets/javascripts/scheduler/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var schedulerApp = angular.module('schedulerApp', ['ngResource', 'ngRoute']);
-schedulerApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+angular.module('schedulerApp', ['ngResource', 'ngRoute'])
+.config(['$routeProvider', '$locationProvider',
+function($routeProvider, $locationProvider){
    $routeProvider.when('/rooms', {
-        templateUrl: '/assets/scheduler/views/rooms/edit.html', 
-        controller: 'RoomsCtrl' 
+        templateUrl: '/assets/scheduler/views/rooms/edit.html',
+        controller: 'RoomsCtrl'
     })
    .otherwise({
         redirectTo: '/presentations'
