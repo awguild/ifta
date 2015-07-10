8f506dd424dbf98e0788c964c477cb72b52e81ca
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-09 20:50:09 -0500

    remove update action from schedule

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -11,22 +11,6 @@ module Api
         render json: @slots
       end
 
-      def update
-        @schedule = @conference.schedule
-        authorize! :update, @schedule
-        if @schedule.update_attributes params[:schedule]
-          redirect_to after_sign_in_path_for(current_user), :notice => 'Schedule successfully updated'
-        else
-          if params[:update_proposals]
-                @slots = @schedule.slots
-                @rooms = @conference.rooms
-                render :action => 'show'
-          else
-            render :action => 'edit'
-          end
-        end
-      end
-
       private
         def load_conference
           @conference = Conference.find_by_conference_year(params[:conference_id])
