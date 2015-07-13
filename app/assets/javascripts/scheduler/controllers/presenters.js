0114653f7b023a0b002c3698f7b987f4207a8940
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-12 21:42:29 -0500

    add a presenters controller that loads presenter data

--- app/assets/javascripts/scheduler/controllers/presenters.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var presentationsCtrl = angular.module('schedulerApp');
-
-presentationsCtrl.controller('PresentationsCtrl', ["$scope", function($scope){
-}]);
--- scheduler_specs/controllers/presentersCtrlSpec.js
+++ scheduler_specs/controllers/presentersCtrlSpec.js
@@ -1,5 +1,35 @@
 describe('PresentersCtrl', function(){
-  it('todo', function(){
-    expect('Everything is fine').toEqual('Everything is fine')
-  })
+  beforeEach(module('schedulerApp'));
+
+  var $q, $scope, $window, presentersCtrl, Presenters, deffered;
+  beforeEach(inject(function($rootScope, _$window_, _$q_, $controller, _Presenters_){
+    Presenters = _Presenters_;
+    $window = _$window_;
+    $q = _$q_;
+
+    deffered = $q.defer();
+    spyOn(Presenters, 'fetch').and.returnValue(deffered.promise);
+
+    $scope = $rootScope.$new();
+    presentersCtrl = $controller('PresentersCtrl', { $scope: $scope});
+  }));
+
+  describe('fetch presentesr', function(){
+    it('should fetch presenters and set them on the controller', function(){
+      var presenters = {email: 'jdoe@test.com'};
+      deffered.resolve({data: presenters});
+      $scope.$digest();
+
+      expect(presentersCtrl.presenters).toEqual(presenters);
+    });
+
+    it('should alert the user if the presenters do not load', function(){
+      spyOn($window, 'alert');
+      deffered.reject('error');
+      expect(function(){
+        $scope.$digest();
+      }).toThrow();
+      expect($window.alert).toHaveBeenCalled();
+    });
+  });
 });
