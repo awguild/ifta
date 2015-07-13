a1e1dd0d90bfec92363db1b158a45e78cb7e03c9
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-13 00:48:42 -0500

    fetch schedule and display

--- app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
+++ app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
@@ -4,6 +4,13 @@ function(Config, Schedule, Slots){
   var self = this;
   this.Config = Config;
 
+  this.time_blocks = []
+  Schedule.fetch()
+  .then(function(res){
+    self.time_blocks = res.data;
+  });
+
+
   this.createSlots = function createSlots(params){
     Slots.bulkCreate(params)
     .then(function(res){
