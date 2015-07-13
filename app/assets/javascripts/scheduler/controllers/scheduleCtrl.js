288dfc548cb7476376608f8e3d52005b5d4f8fe1
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-13 00:48:14 -0500

    add a form for creating slots and time blocks

--- app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
+++ app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
@@ -1,4 +1,14 @@
 angular.module('schedulerApp')
-.controller('ScheduleCtrl', ['Config', function(Config){
+.controller('ScheduleCtrl', ['Config', 'Schedule', 'Slots',
+function(Config, Schedule, Slots){
+  var self = this;
   this.Config = Config;
+
+  this.createSlots = function createSlots(params){
+    Slots.bulkCreate(params)
+    .then(function(res){
+      // todo smarter push logic, merge if existing block
+      self.time_blocks.push(res.data)
+    })
+  }
 }]);
