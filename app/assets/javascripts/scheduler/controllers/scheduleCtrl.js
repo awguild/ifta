192ba94c758ddc3c0f6cb5e0c8381f5095f9fe66
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-13 21:57:45 -0500

    add schedule controller for loading the schedule and updating a slot

--- app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
+++ app/assets/javascripts/scheduler/controllers/scheduleCtrl.js
@@ -1,21 +1,31 @@
 angular.module('schedulerApp')
-.controller('ScheduleCtrl', ['Config', 'Schedule', 'Slots',
-function(Config, Schedule, Slots){
+.controller('ScheduleCtrl', ['Config', 'Schedule', 'Slots', 'Slot',
+function(Config, Schedule, Slots, Slot){
   var self = this;
   this.Config = Config;
-
+  this.errorMessage = null;
   this.time_blocks = []
+
+  /*
+   * fetches an array with the full schedule structured like
+   * time_blocks []
+   *  slots []
+   *    proposal {}
+   *      user {}
+   *        country {}
+   *    room {}
+   */
   Schedule.fetch()
   .then(function(res){
     self.time_blocks = res.data;
+  }).catch(function(err){
+    self.errorMessage = 'Unable to fetch the schedule.'
   });
 
-
-  this.createSlots = function createSlots(params){
-    Slots.bulkCreate(params)
-    .then(function(res){
-      // todo smarter push logic, merge if existing block
-      self.time_blocks.push(res.data)
+  /* updates a single slot */
+  this.updateSlot = function updateSlot(params){
+    Slot.update({id: params.id}, params).catch(function(err){
+      self.errorMessage = 'Unable to update slot.'
     })
   }
 }]);
--- scheduler_specs/controllers/scheduleCtrlSpec.js
+++ scheduler_specs/controllers/scheduleCtrlSpec.js
@@ -1,9 +1,21 @@
 describe('ScheduleCtrl', function(){
   beforeEach(module('schedulerApp'));
 
-  var scheduleCtrl, Config, $scope;
-  beforeEach(inject(function($rootScope, $controller, _Config_){
+  beforeEach(module(function($provide){
+    $provide.value('Config', {conferenceYear: '2015'});
+  }));
+
+  var scheduleCtrl, Config, $scope, $q, $httpBackend, Slot;
+  beforeEach(inject(function($rootScope, $controller, $injector, _$q_, _Config_, _Slot_){
     Config = _Config_;
+    Slot = _Slot_;
+    $q = _$q_;
+
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.whenGET('/api/v1/conferences/2015/schedule')
+    .respond(200, "[]")
+    $httpBackend.whenGET('/api/v1/conferences/2016/schedule')
+    .respond(500, "[]")
 
     $scope = $rootScope.$new();
     scheduleCtrl = $controller('ScheduleCtrl', { $scope: $scope});
@@ -11,5 +23,39 @@ describe('ScheduleCtrl', function(){
 
   it('should make Config available', function(){
     expect(scheduleCtrl.Config).toEqual(Config)
+  });
+
+
+  describe('Schedule.fetch', function(){
+    it('should have fetched the schedule and assigned it', function(){
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+
+      expect(scheduleCtrl.time_blocks).toEqual([]);
+    });
+  });
+
+  describe('updateSlot', function(){
+    it('should update the slot', function(){
+      var params = {id: 3, proposal_id: 7}
+        , deffered = $q.defer();
+
+      spyOn(Slot, 'update').and.returnValue(deffered.promise)
+      scheduleCtrl.updateSlot(params)
+      expect(Slot.update).toHaveBeenCalledWith({id: 3}, params);
+    });
+
+    it('should add an errorMessage when unable to update the slot', function(){
+      var params = {id: 3, proposal_id: 7}
+        , deffered = $q.defer();
+
+      spyOn(Slot, 'update').and.returnValue(deffered.promise);
+      scheduleCtrl.updateSlot(params);
+      deffered.reject('error');
+      $scope.$digest();
+
+      expect(scheduleCtrl.errorMessage).not.toBe(null);
+    });
   });
 });
