2ecb011638442dc0514d1738b1551cfceb157e62
Ben Fischer <ben.fischer.810@gmail.com>
2015-07-15 21:53:05 -0500

    ann angular route module and some routes

--- app/assets/javascripts/scheduler/app.js
+++ app/assets/javascripts/scheduler/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-angular.module('schedulerApp', ['ngResource']);
+angular.module('schedulerApp', ['ngResource', 'ngRoute'])
+.config(['$routeProvider', '$locationProvider',
+function($routeProvider, $locationProvider){
+   $routeProvider
+   .when('/', {
+      templateUrl: 'schedule.html',
+      controller: 'ScheduleCtrl'
+   })
+   .when('/rooms', {
+        templateUrl: 'rooms.html',
+        controller: 'RoomsCtrl'
+    })
+   .when('/time_blocks', {
+        templateUrl: 'time_blocks.html',
+        controller: 'TimeBlocksCtrl'
+    })
+   .otherwise({
+      redirectTo: '/'
+    })
+}]);
