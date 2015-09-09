bed8ae4d721b33aae34748fbc21c3a09a615fd49
Ben Fischer <ben.fischer.810@gmail.com>
2015-09-09 08:44:08 -0500

    define empty show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def show
+  end
+
   def index
     @conference = selected_conference
     @conference_items = @conference.conference_items
