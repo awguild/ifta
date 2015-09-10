a043d48748d855ef567d813ebcc9abb4f8f3e689
Ben Fischer <ben.fischer.810@gmail.com>
2015-09-09 21:46:36 -0500

    don't load dotenv manually

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-Dotenv::Railtie.load
+
 module Iftaconferenceapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
