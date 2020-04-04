b690053fd4e7c7ee6f30c9d2adf66719afbb04f7
Ben Fischer <ben.fischer.810@gmail.com>
2020-04-04 15:01:29 -0500

    remove skip for undefined action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
-  skip_before_action :check_contact_info
   #let the general public access the index page
 
   def index
