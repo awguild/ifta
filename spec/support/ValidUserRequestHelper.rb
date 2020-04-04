084c68c9e1d2943d26bfe110f243c0bf8411e01d
Ben Fischer <ben.fischer.810@gmail.com>
2020-04-04 12:48:40 -0500

    deprecation post_via_redirect to follow_redirect!

--- spec/support/ValidUserRequestHelper.rb
+++ spec/support/ValidUserRequestHelper.rb
@@ -4,12 +4,14 @@ module ValidUserRequestHelper
   # for use in request specs
   def sign_in_as_a_valid_user
     @user ||= create(:user)
-    post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+    post user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+    follow_redirect!
   end
 
   def sign_in_as_a_admin_user
     @user ||= create(:user, :admin)
-    post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+    post user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+    follow_redirect!
   end
 
   def create_conference
