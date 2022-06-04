c8bf8c2ab0a54f1f0951689b23d0bdd4f89160a7
Ben Fischer <ben.fischer.810@gmail.com>
2022-06-04 09:53:27 -0500

    fix migration

--- db/migrate/20220520014059_add_non_english_long_des_proposals.rb
+++ db/migrate/20220520014059_add_non_english_long_des_proposals.rb
@@ -1,4 +1,4 @@
-class AddNonEnglishProposals < ActiveRecord::Migration[6.0]
+class AddNonEnglishLongDesProposals < ActiveRecord::Migration[6.0]
   def change
     add_column :proposals, :long_description_non_english, :text
   end
