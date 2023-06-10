b566846b690ae1a44a7c3d2409289a3dc76f951f
Ben Fischer <ben.fischer.810@gmail.com>
2023-06-10 18:50:11 -0500

    fix migration

--- db/migrate/20230610210129_add_japanese_and_korean.rb
+++ db/migrate/20230610210129_add_japanese_and_korean.rb
@@ -1,4 +1,4 @@
-class AddJapaneseAndKorean < ActiveRecord::Migration
+class AddJapaneseAndKorean < ActiveRecord::Migration[6.0]
     def up
       add_column :proposals, :language_japanese, :boolean, after: :language_mandarin
       add_column :proposals, :language_korean, :boolean, after: :language_japanese
