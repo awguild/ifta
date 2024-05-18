656c1e86e1db04b684d6209f412961fefa0f47d7
Ben Fischer <ben.fischer.810@gmail.com>
2024-05-18 16:13:54 -0400

    remove sprockets-rails

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,6 @@ module Iftaconferenceapp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.assets.css_compressor = nil
   end
 end
