ad3c9426f4a5f40c78d3f0ec8c787c7a612f36d6
Ben Fischer <ben.fischer.810@gmail.com>
2024-09-21 12:44:33 -0400

    fix coffeescript -> JS bug

--- app/assets/javascripts/proposals.js
+++ app/assets/javascripts/proposals.js
@@ -1,14 +1,14 @@
 jQuery(() => {
   $('form').on('click', '.remove_fields', (event) => {
-    $(this).prev('input[type=hidden]').val('1')
-    $(this).closest('fieldset').hide()
+    $(event.target).prev('input[type=hidden]').val('1')
+    $(event.target).closest('fieldset').hide()
     event.preventDefault()
   })
 
   $('form').on('click', '.add_fields', (event) => {
     time = new Date().getTime()
-    regexp = new RegExp($(this).data('id'), 'g')
-    $(this).before($(this).data('fields').replace(regexp, time))
+    regexp = new RegExp($(event.target).data('id'), 'g')
+    $(event.target).before($(event.target).data('fields').replace(regexp, time))
     event.preventDefault()
   })
 });
