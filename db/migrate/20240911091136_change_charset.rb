b8ff34f9996dce46a74d5a6bf8f28117aa38cc10
Ben Fischer <ben.fischer.810@gmail.com>
2024-09-21 12:44:15 -0400

    update charset for graduating_institution

--- db/migrate/20240911091136_change_charset.rb
+++ db/migrate/20240911091136_change_charset.rb
@@ -5,6 +5,7 @@ class ChangeCharset < ActiveRecord::Migration[6.0]
       ALTER TABLE presenters MODIFY COLUMN last_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE presenters MODIFY COLUMN email VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE presenters MODIFY COLUMN affiliation_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+      ALTER TABLE presenters MODIFY COLUMN graduating_institution VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE users MODIFY COLUMN first_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE users MODIFY COLUMN last_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE proposals MODIFY COLUMN title text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
